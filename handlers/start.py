d47f7b73f7a1daab789a65be54e2dda1893fc768
uditvarshney100 <81468435+uditvarshney100@users.noreply.github.com>
2021-04-15 21:06:02 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -13,8 +13,8 @@ async def start_(client: Client, message: Message):
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
 \nI can play music in your group's voice chat
-Maintained by @HEROGAMERS1 ❤
-\nTo add in your group contact us at @LaylaSupport.
+Maintained by @uditvarshney100
+\nfor any queery contact us @bothousesupport
 \nUse the buttons below to know more about me.
  </b>""",
       
@@ -23,23 +23,20 @@ Maintained by @HEROGAMERS1 ❤
             [
                 [
                     InlineKeyboardButton(
-                        "🌍 Music World", url="https://t.me/GIRLS_AND_BOYS_CHATTING",
+                        "🌍 Music World", url="https://t.me/bothousesupport",
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/LaylaSupport"
+                        "💬 Group", url="https://t.me/bothousesupport"
                     ),
                     InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/LaylaList"
-                    ),
-                    InlineKeyboardButton(
-                        "💾 Source code", url="https://github.com/QueenArzoo/VCPlayBot"
-                    )
+                        "🔊 Channel", url="https://t.me/bothouse100"
+                    )                                                     
                 ],
                 [
                     InlineKeyboardButton(
-                        "➕ Add To Your Group ➕", url="https://t.me/VCPlayBot?startgroup=true"
+                        "➕ Add To Your Group ➕", url="https://t.me/musicbaba100_bot?startgroup=true"
                     )
                 ]
             ]
